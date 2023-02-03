3a59dbc9b88c187b8e44ae939e054130b9aab3f2
danielOuattara <daniel.ouattara@gmx.com>
2023-02-03 15:20:30 +0100

    Style, Footer , Pages and Navbar

---
+++ 02_SimplyRecipes_Project_Gatsby_5/02_07_Style_Footer_And_Pages_Navbar/src/components/Header.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const Header = () => {
+  return (
+    <footer>
+      <p2>Hello from header</p2>
+    </footer>
+  );
+};
+
+export default Header;
---
+++ 02_SimplyRecipes_Project_Gatsby_5/02_07_Style_Footer_And_Pages_Navbar/src/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "./../components/Layout";
+
+export default function Home() {
+  return (
+    <div>
+      <Layout>
+        <h1>Home Page</h1>
+      </Layout>
+    </div>
+  );
+}
