188689972cc109b7fe539d8dde897d24e7ba20b8
danielOuattara <daniel.ouattara@gmx.com>
2023-02-03 14:15:36 +0100

    udpate styling strategies

--- 02_SimplyRecipes_Project_Gatsby_5/02_05_styling_strategies/src/pages/about.js
+++ 02_SimplyRecipes_Project_Gatsby_5/02_05_styling_strategies/src/pages/about.js
@@ -1,10 +1,29 @@
 import React from "react";
 import Layout from "./../components/Layout";
+import { page, text } from "./../style/About.module.css";
+import styled from "styled-components";
 
 export default function About() {
   return (
     <Layout>
-      <h2 className="about-heading">About heading</h2>
+      <div className={page}>
+        <h1> Title of about - styleing purpose</h1>
+        <h2 className="about-heading">About heading</h2>
+        <p className={text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+          placeat ratione facilis laudantium fugiat possimus explicabo, officia
+          molestiae unde, dolore deleniti impedit cumque ad quae fugit aliquam
+          hic aut quidem.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
+          quae, deserunt pariatur eligendi expedita ad hic, iste quis quod non
+          numquam mollitia accusantium! Eos nobis nostrum placeat cupiditate
+          doloremque dolorum.
+        </p>
+      </div>
     </Layout>
   );
 }
+
+const Wrapper = styled.section``;
--- 02_SimplyRecipes_Project_Gatsby_5/02_05_styling_strategies/src/pages/index.js
+++ 02_SimplyRecipes_Project_Gatsby_5/02_05_styling_strategies/src/pages/index.js
@@ -1,5 +1,7 @@
 import * as React from "react";
 import Layout from "./../components/Layout";
+import { page, text } from "./../style/Home.module.css";
+import { ExampleButton } from "../styled-component/ExampleButton";
 
 export default function Home() {
   return (
@@ -8,8 +10,9 @@ export default function Home() {
         <h1 style={{ color: "green", textTransform: "capitalize" }}>
           Hello world!{" "}
         </h1>
+        <ExampleButton>Click on Button Styled Component</ExampleButton>
         <h2 className="home-heading">Hello world Again! </h2>
-        <p>
+        <p className={(page, text)}>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa
           excepturi nisi quia. Nihil doloribus architecto recusandae officia ad
           quibusdam impedit eligendi sapiente culpa minus, velit magni, beatae
