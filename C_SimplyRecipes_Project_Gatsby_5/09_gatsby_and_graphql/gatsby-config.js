dae678e062afff44024b7ac71a58512f2db88909
danielOuattara <daniel.ouattara@gmx.com>
2023-02-07 20:39:51 +0100

    gatsby & grapql : starting

---
+++ C_SimplyRecipes_Project_Gatsby_5/09_gatsby_and_graphql/gatsby-config.js
@@ -0,0 +1,58 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, // Needed for dynamic images
+  ],
+  siteMetadata: {
+    title: `Simple recipes`,
+    description: `Nice and clean recipes site`,
+    inspiredBy: `John smilga`,
+    author: {
+      name: `daniel.ouattara`,
+      place: `Combs la ville, France`,
+    },
+    simpleData: [`item 1`, `item 2`],
+    complexData: [
+      {
+        id: 1,
+        first_name: `Katlin`,
+        last_name: `Sudy`,
+        email: `ksudy0@github.io`,
+        gender: `Genderqueer`,
+        ip_address: `28.40.93.31`,
+      },
+      {
+        id: 2,
+        first_name: `Baryram`,
+        last_name: `Rubinovitch`,
+        email: `brubinovitch1@mapquest.com`,
+        gender: `Male`,
+        ip_address: `82.141.109.32`,
+      },
+      {
+        id: 3,
+        first_name: `Julius`,
+        last_name: `Hue`,
+        email: `jhue2@bandcamp.com`,
+        gender: `Male`,
+        ip_address: `145.143.44.46`,
+      },
+    ],
+  },
+};
