aa4a37cc37e65c8b9308933fcedb82fa0e55f956
danielOuattara <daniel.ouattara@gmx.com>
2023-02-07 17:16:59 +0100

    gatsby image workflow and cases analyse

---
+++ C_SimplyRecipes_Project_Gatsby_5/08_images/gatsby-config.js
@@ -0,0 +1,22 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, // Needed for dynamic images
+  ],
+};
