a9f4ef0507ffb6d1a49690ddabb40201cd21acea
danielOuattara <daniel.ouattara@gmx.com>
2023-08-25 18:14:43 +0200

    example logic update

--- C_SimplyRecipes_Project_Gatsby_5/08_Images_in_Gatsby/src/components/examples/ImagesExample2.js
+++ C_SimplyRecipes_Project_Gatsby_5/08_Images_in_Gatsby/src/components/examples/ImagesExample2.js
@@ -1,48 +1,52 @@
 import React from "react";
-import "./images_Example_2.module.css";
+// import "./images_Example_2.module.css";
+import * as styles from "./images_Example_2.module.css";
 import { StaticImage } from "gatsby-plugin-image";
 
 export default function Images_Example_2() {
   return (
-    <section>
-      <>
-        {/* <h4> fixed </h4> */}
+    <section className={styles.section}>
+      <article className={styles.article}>
+        <h4> constrained / default</h4>
+        <StaticImage
+          src="./../../assets/images/recipe-1.jpeg"
+          alt="recipe 1"
+          placeholder="tracedSVG"
+          layout="constrained"
+          // height={400}
+          // width={200}
+          className={styles.exampleImg}
+          as="section"
+        />
+      </article>
+
+      <article className={styles.article}>
+        <h4> fixed </h4>
         <StaticImage
           src="./../../assets/images/recipe-1.jpeg"
           alt="recipe 1"
           placeholder="blurred"
           layout="fixed"
-          // width={200}
-          // className="example-img"
+          // height={400}
+          width={200}
+          className={styles.exampleImg}
           as="div"
         />
-      </>
+      </article>
 
-      <>
-        {/* <h4> constrained / default</h4> */}
-        <StaticImage
-          src="./../../assets/images/recipe-1.jpeg"
-          alt="recipe 1"
-          // height={400} // toggle
-          placeholder="tracedSVG"
-          layout="constrained"
-          // className="example-img"
-          as="footer"
-        />
-      </>
-
-      <>
-        {/* <h4> full width </h4> */}
+      <article className={styles.article}>
+        <h4> full width</h4>
         <StaticImage
           src="./../../assets/images/recipe-1.jpeg"
           alt="recipe 1"
           placeholder="dominantColor"
           layout="fullWidth"
-          // className="example-img"
+          className={styles.exampleImg}
+          // height={400}
+          // width={200}
           as="article"
-          // width={300} //toggle
         />
-      </>
+      </article>
     </section>
   );
 }
