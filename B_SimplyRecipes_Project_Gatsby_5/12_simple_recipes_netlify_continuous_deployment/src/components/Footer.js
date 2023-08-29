360b487b01d70015f436b49c02b370751cae8818
danielOuattara <daniel.ouattara@gmx.com>
2023-08-30 01:54:42 +0200

    update readme + footer info & style

--- B_SimplyRecipes_Project_Gatsby_5/12_simple_recipes_netlify_continuous_deployment/src/components/Footer.js
+++ B_SimplyRecipes_Project_Gatsby_5/12_simple_recipes_netlify_continuous_deployment/src/components/Footer.js
@@ -1,14 +1,21 @@
 import * as React from "react";
 
-const Footer = () => {
+export default function Footer() {
   return (
-    <footer className="page-footer">
+    <footer className="page-footer ">
       <p>
         &copy; {new Date().getFullYear()} <span>SimpleRecipes</span> Powered
-        with <a href="https://www.gatsbyjs.com/">gatsby</a>
+        with <a href="https://www.gatsbyjs.com/">Gatsby 5+</a>
+      </p>
+      <p>
+        <a
+          href="https://github.com/danielOuattara/Gatsby_John_Smilga_Udemy/tree/master/B_SimplyRecipes_Project_Gatsby_5/12_simple_recipes_netlify_continuous_deployment"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span>GitHub Source Code</span>
+        </a>
       </p>
     </footer>
   );
-};
-
-export default Footer;
+}
