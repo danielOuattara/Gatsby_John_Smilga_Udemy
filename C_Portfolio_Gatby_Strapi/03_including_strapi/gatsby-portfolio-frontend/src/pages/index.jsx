6aa10eae058e6ee02a05963bfa55f9437563b60c
danielOuattara <daniel.ouattara@gmx.com>
2023-09-01 11:39:48 +0200

    C: 03  fetch and display  featured projects

--- C_Portfolio_Gatby_Strapi/03_including_strapi/gatsby-portfolio-frontend/src/pages/index.jsx
+++ C_Portfolio_Gatby_Strapi/03_including_strapi/gatsby-portfolio-frontend/src/pages/index.jsx
@@ -2,15 +2,49 @@ import React from "react";
 import { graphql } from "gatsby";
 import { Hero, Services, Jobs, Projects, Seo, Navbar } from "../components";
 
-export default function IndexPage() {
+export default function IndexPage(props) {
+  const projects = props.data.allStrapiProject.nodes;
+
   return (
     <>
       <main>
         <Hero />
         <Services />
         <Jobs />
-        <Projects />
+        <Projects
+          title="featured projects"
+          showLinkToProjects
+          projects={projects}
+        />
       </main>
     </>
   );
 }
+
+export const query = graphql`
+  query {
+    allStrapiProject(filter: { featured: { eq: true } }) {
+      totalCount
+      nodes {
+        id
+        slug
+        title
+        featured
+        description
+        github
+        url
+        stack {
+          id
+          title
+        }
+        image {
+          localFile {
+            childImageSharp {
+              gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+            }
+          }
+        }
+      }
+    }
+  }
+`;
