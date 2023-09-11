7a238ca135cb41a9b02d01a5c9fce0b8aa0c2aed
danielOuattara <daniel.ouattara@gmx.com>
2023-09-11 17:50:54 +0200

    page links in Navbar

--- C_Portfolio_Gatsby_Strapi/gatsby-client/src/components/Navbar.jsx
+++ C_Portfolio_Gatsby_Strapi/gatsby-client/src/components/Navbar.jsx
@@ -5,5 +5,23 @@ import { page_links } from "../constants";
 import { Link } from "gatsby";
 
 export default function Navbar() {
-  return <h2>navbar component</h2>;
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="web dev" />
+          <button type="button" className="toggle-btn">
+            <FaAlignRight />
+          </button>
+        </div>
+        <div className="nav-links">
+          {page_links.map((link) => (
+            <Link key={link.id} to={link.url}>
+              {link.text}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </nav>
+  );
 }
