3ead8fe52e5f580e87b0af33e7dc1098ef3a14d9
danielOuattara <daniel.ouattara@gmx.com>
2023-09-11 22:47:50 +0200

    117 gatsby: set single job

--- C_Portfolio_Gatsby_Strapi/portfolio-gatsby-client/src/components/Jobs.jsx
+++ C_Portfolio_Gatsby_Strapi/portfolio-gatsby-client/src/components/Jobs.jsx
@@ -23,6 +23,28 @@ export const query = graphql`
 
 export default function Jobs() {
   const data = useStaticQuery(query);
-  console.log("data = ", data);
-  return <h2>jobs component</h2>;
+  const jobs = data.allStrapiJob.nodes;
+
+  return (
+    <section className="section jobs">
+      <Title title="experience" />
+      <div className="jobs-center">
+        <div className="btn-container"></div>
+        <article className="job-info">
+          <h3>{jobs[0].position}</h3>
+          <h4>{jobs[0].company}</h4>
+          <p className="job-date">{jobs[0].date}</p>
+          {jobs[0].description.map((item) => (
+            <div key={item.id} className="job-desc">
+              <FaAngleDoubleRight className="job-icon" />
+              <p>{item.name}</p>
+            </div>
+          ))}
+        </article>
+      </div>
+      <Link to="/about" className="btn center-btn">
+        more info
+      </Link>
+    </section>
+  );
 }
