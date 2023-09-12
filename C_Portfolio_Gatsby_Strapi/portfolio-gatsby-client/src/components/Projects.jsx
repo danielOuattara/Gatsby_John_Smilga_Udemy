f1a6fbf04c4ad26c1284d5db76f590a6890ecc12
danielOuattara <daniel.ouattara@gmx.com>
2023-09-12 10:08:25 +0200

    124 gatsby Projects component

--- C_Portfolio_Gatsby_Strapi/portfolio-gatsby-client/src/components/Projects.jsx
+++ C_Portfolio_Gatsby_Strapi/portfolio-gatsby-client/src/components/Projects.jsx
@@ -1,8 +1,21 @@
 import React from "react";
-import Title from "./Title";
-import Project from "./Project";
+import { Title, Project } from "./index";
 import { Link } from "gatsby";
 
-export default function Projects() {
-  return <h2>all projects component</h2>;
+export default function Projects(props) {
+  return (
+    <section className="section projects">
+      <Title title={props.title} />
+      <div className="section-center projects-center">
+        {props.projects.map((project, index) => (
+          <Project key={project.id} index={index} {...project}></Project>
+        ))}
+      </div>
+      {props.showLinkToProjects && (
+        <Link to="/projects" className="btn center-btn">
+          projects
+        </Link>
+      )}
+    </section>
+  );
 }
