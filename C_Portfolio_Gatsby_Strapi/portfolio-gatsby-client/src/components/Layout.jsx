a1a5d836898351254c3131cbf9c621e6996a1a81
danielOuattara <daniel.ouattara@gmx.com>
2023-09-13 20:48:13 +0200

    134&135: sidebar setup + logic OK

--- C_Portfolio_Gatsby_Strapi/portfolio-gatsby-client/src/components/Layout.jsx
+++ C_Portfolio_Gatsby_Strapi/portfolio-gatsby-client/src/components/Layout.jsx
@@ -1,11 +1,17 @@
-import React from "react";
-import { Navbar, Sidebar, Footer } from "./../components";
-
+import React, { useState } from "react";
+import { Navbar, Sidebar, Footer } from "./";
 import "../assets/css/main.css";
+
 export default function Layout(props) {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
   return (
     <>
-      <Navbar />
+      <Navbar toggleSidebar={toggleSidebar} />
+      <Sidebar toggleSidebar={toggleSidebar} isSidebarOpen={isSidebarOpen} />
       {props.children}
       <Footer />
     </>
