534bde35f86a058553a350faa4a19130facd8459
danielOuattara <daniel.ouattara@gmx.com>
2023-09-13 19:26:40 +0200

    132 error page & Footer component

--- C_Portfolio_Gatsby_Strapi/portfolio-gatsby-client/src/pages/404.jsx
+++ C_Portfolio_Gatsby_Strapi/portfolio-gatsby-client/src/pages/404.jsx
@@ -1,13 +1,20 @@
-import React from "react"
-import { Link } from "gatsby"
-import Seo from "../components/Seo"
+import React from "react";
+import { Link } from "gatsby";
 
-const Error = () => {
+//-----------------------------------------------
+
+export default function Error() {
   return (
     <>
-      <h2>error page</h2>
+      <main className="error-page">
+        <div className="error-container">
+          <h1>404</h1>
+          <h3>page not found ;(</h3>
+          <Link to="/" className="btn">
+            back safely to home
+          </Link>
+        </div>
+      </main>
     </>
-  )
+  );
 }
-
-export default Error
