b4fd02766005e43e19c77b8a3014085c048470aa
danielOuattara <daniel.ouattara@gmx.com>
2023-09-13 19:17:40 +0200

    131 about page completed

--- C_Portfolio_Gatsby_Strapi/portfolio-gatsby-client/src/pages/about.jsx
+++ C_Portfolio_Gatsby_Strapi/portfolio-gatsby-client/src/pages/about.jsx
@@ -1,14 +1,48 @@
-import React from "react"
-import { graphql } from "gatsby"
-import Title from "../components/Title"
-import Seo from "../components/Seo"
+import React from "react";
+import { graphql } from "gatsby";
+import { Title } from "../components";
 
-const About = () => {
+//------------------------------------------------------
+export default function About(props) {
+  const aboutData = props.data.strapiAbout;
+
   return (
-    <>
-      <h2>about page</h2>
-    </>
-  )
+    <section className="about-page">
+      <div className="section-center about-center">
+        <img
+          src={aboutData.image.localFile.publicURL}
+          alt={aboutData.title}
+          className="about-img"
+        />
+        <article className="about-text">
+          <Title title={aboutData.title} />
+          <p>{aboutData.info}</p>
+          <div className="about-stack">
+            {aboutData.stack.map((item) => (
+              <span key={item.id}>{item.title}</span>
+            ))}
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 }
 
-export default About
+//------------------------------------------------------
+export const query = graphql`
+  query {
+    strapiAbout {
+      title
+      info
+      stack {
+        id
+        title
+      }
+      image {
+        localFile {
+          publicURL
+        }
+      }
+    }
+  }
+`;
