a1ae3ec52b54de68ef1529b912ade0b3d9762ea2
danielOuattara <daniel.ouattara@gmx.com>
2023-09-13 19:32:02 +0200

    133 contact page

--- C_Portfolio_Gatsby_Strapi/portfolio-gatsby-client/src/pages/contact.jsx
+++ C_Portfolio_Gatsby_Strapi/portfolio-gatsby-client/src/pages/contact.jsx
@@ -1,11 +1,41 @@
-import React from "react"
-import Seo from "../components/Seo"
-const contact = () => {
+import React from "react";
+
+export default function Contact() {
   return (
     <>
-      <h2>contact page</h2>
+      <section className="contact-page">
+        <article className="contact-form">
+          <h3>stay in touch ?</h3>
+          <form>
+            <div className="form-group">
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="name"
+                className="form-control"
+              />
+              <input
+                type="email"
+                name="email"
+                id="email"
+                placeholder="email"
+                className="form-control"
+              />
+              <textarea
+                name="message"
+                id="message"
+                rows="7"
+                className="form-control"
+                placeholder="message"
+              ></textarea>
+            </div>
+            <button type="submit" className="submit-btn btn">
+              submit
+            </button>
+          </form>
+        </article>
+      </section>
     </>
-  )
+  );
 }
-
-export default contact
