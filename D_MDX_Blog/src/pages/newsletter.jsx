f00275a475829ee6e258361a193e6c6b11235c01
danielOuattara <daniel.ouattara@gmx.com>
2023-09-18 17:34:18 +0200

    152 Newsletter form OK

--- D_MDX_Blog/src/pages/newsletter.jsx
+++ D_MDX_Blog/src/pages/newsletter.jsx
@@ -1,6 +1,32 @@
 import React from "react";
-import Layout from "../components/Layout";
+import { Layout } from "../components";
 
 export default function NewsLetter() {
-  return <h4>newsletter</h4>;
+  return (
+    <Layout>
+      <section className="newsletter-page">
+        <div className="page-center">
+          <h2>Get all latest stories to your inbox</h2>
+          <h4>I write to my friends every few weeks</h4>
+          <form className="contact-form">
+            <input
+              type="text"
+              name="name"
+              placeholder="Your name"
+              className="form-control"
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Your email"
+              className="form-control"
+            />
+            <button type="submit" className="form-control btn">
+              Subscribe
+            </button>
+          </form>
+        </div>
+      </section>
+    </Layout>
+  );
 }
