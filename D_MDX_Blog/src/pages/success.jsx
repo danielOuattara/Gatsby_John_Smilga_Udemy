1413392702a2abc0725c6a41420eff2e6a8b3188
danielOuattara <daniel.ouattara@gmx.com>
2023-09-18 19:30:08 +0200

    newsLetter + netlify setup fro form. To complete !

--- D_MDX_Blog/src/pages/success.jsx
+++ D_MDX_Blog/src/pages/success.jsx
@@ -3,5 +3,16 @@ import Layout from "../components/Layout";
 import { Link } from "gatsby";
 
 export default function Success() {
-  return <h4>success page</h4>;
+  return (
+    <Layout>
+      <section className="success-page">
+        <div className="page-center">
+          <h2>your submission was successful</h2>
+          <Link to="/" className="btn">
+            Back to home page
+          </Link>
+        </div>
+      </section>
+    </Layout>
+  );
 }
