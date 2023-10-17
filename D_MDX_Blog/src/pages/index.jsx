34cfc7c43bb3267c485fe03b1915976aadd411a7
danielOuattara <daniel.ouattara@gmx.com>
2023-10-17 13:49:32 +0200

    166 Home Query

--- D_MDX_Blog/src/pages/index.jsx
+++ D_MDX_Blog/src/pages/index.jsx
@@ -2,10 +2,34 @@ import React from "react";
 import { Hero, Layout, Posts } from "../components";
 import { graphql } from "gatsby";
 
-export default function IndexPage() {
+export default function IndexPage(props) {
+  console.log(props);
   return (
     <Layout>
       <Hero showPerson />
     </Layout>
   );
 }
+
+export const query = graphql`
+  query {
+    allMdx(limit: 3, sort: { frontmatter: { date: DESC } }) {
+      nodes {
+        id
+        frontmatter {
+          title
+          author
+          category
+          readTime
+          date(formatString: "MMMM, Do YYYY")
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+        excerpt
+      }
+    }
+  }
+`;
