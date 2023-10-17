a353c54a55e3b64e761c8772a5469ae1f5a5c9b7
danielOuattara <daniel.ouattara@gmx.com>
2023-10-17 22:02:04 +0200

    174 Single category JSX setup

--- D_MDX_Blog/src/templates/category-template.jsx
+++ D_MDX_Blog/src/templates/category-template.jsx
@@ -3,31 +3,36 @@ import { Layout, Hero, Posts } from "../components";
 import { graphql } from "gatsby";
 
 export default function CategoryTemplate(props) {
-  console.log(props);
-  return <h2>category template</h2>;
+  return (
+    <Layout>
+      <Hero />
+      <Posts
+        title={`categories / ${props.pageContext.category}`}
+        posts={props.data.allMdx.nodes}
+      />
+    </Layout>
+  );
 }
 
 export const query = graphql`
   query QuerysingleCategory($category: String) {
     allMdx(filter: { frontmatter: { category: { eq: $category } } }) {
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            author
-            category
-            readTime
-            date(formatString: "MMMM, Do YYYY")
-            image {
-              childImageSharp {
-                gatsbyImageData
-              }
-              publicURL
+      nodes {
+        id
+        frontmatter {
+          title
+          author
+          category
+          readTime
+          date(formatString: "MMMM, Do YYYY")
+          image {
+            childImageSharp {
+              gatsbyImageData
             }
+            publicURL
           }
-          excerpt
         }
+        excerpt
       }
     }
   }
