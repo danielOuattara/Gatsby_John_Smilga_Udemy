9ac813ef3dfbb0d673292e6b6397d27c812cb774
danielOuattara <daniel.ouattara@gmx.com>
2023-10-17 22:34:49 +0200

    176 Banner Recent Posts

--- D_MDX_Blog/src/components/Banner/Recent.jsx
+++ D_MDX_Blog/src/components/Banner/Recent.jsx
@@ -1,19 +1,51 @@
-import React from 'react'
-import styled from 'styled-components'
-import { graphql, useStaticQuery } from 'gatsby'
-import { Link } from 'gatsby'
-import { GatsbyImage, getImage } from 'gatsby-plugin-image'
-import Title from './Title'
+import React from "react";
+import styled from "styled-components";
+import { graphql, useStaticQuery } from "gatsby";
+import { Link } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import Title from "./Title";
 
+const query = graphql`
+  query QueryRecentsPosts {
+    allMdx(limit: 5, sort: { frontmatter: { date: DESC } }) {
+      nodes {
+        id
+        frontmatter {
+          title
+          slug
+          date(formatString: "MMMM, Do YYYY")
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
-
-const Recent = () => {
-  
+export default function Recent() {
+  const data = useStaticQuery(query);
+  // console.log(data);
   return (
     <Wrapper>
-      Banner Recent
+      <Title title={"recent posts"} />
+      {data.allMdx.nodes.map((post) => (
+        <Link to={`/posts/${post.frontmatter.slug}`} className="post">
+          <GatsbyImage
+            image={getImage(post.frontmatter.image)}
+            alt={post.frontmatter.title}
+            className="img"
+          />
+          <div>
+            <h5>{post.frontmatter.title}</h5>
+            <p>{post.frontmatter.date}</p>
+          </div>
+        </Link>
+      ))}
     </Wrapper>
-  )
+  );
 }
 
 const Wrapper = styled.div`
@@ -43,6 +75,4 @@ const Wrapper = styled.div`
       color: var(--clr-primary-5);
     }
   }
-`
-
-export default Recent
+`;
