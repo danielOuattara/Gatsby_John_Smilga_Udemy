1e15629b3c671bfbc2c219595a7fa97b3a534b4c
danielOuattara <daniel.ouattara@gmx.com>
2023-10-24 16:37:34 +0200

    201 Root Wrapper in gatsby-browser & gatsby-ssr

--- E_Design_Project_AirTable/gatsby-browser.js
+++ E_Design_Project_AirTable/gatsby-browser.js
@@ -1,19 +1,3 @@
-import React from "react";
-import { createGlobalStyle } from "styled-components";
+import { wrapRootElement as globalWrapper } from "./rootWrapper";
 
-const GlobalStyle = createGlobalStyle`
-h2{
-    color: red;
-    font-size: 4rem;
-}
-`;
-
-export function wrapRootElement({ element }) {
-  console.log("SUPER WRAPPER");
-  return (
-    <>
-      <GlobalStyle />
-      {element}
-    </>
-  );
-}
+export const wrapRootElement = globalWrapper;
---
+++ E_Design_Project_AirTable/gatsby-ssr.js
@@ -0,0 +1,3 @@
+import { wrapRootElement as globalWrapper } from "./rootWrapper";
+
+export const wrapRootElement = globalWrapper();
