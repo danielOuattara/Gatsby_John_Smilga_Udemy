bb047c4300258caf22c40b50d2670ab968de2a46
danielOuattara <daniel.ouattara@gmx.com>
2023-10-24 16:05:53 +0200

    196-199: Starting Design Project Airtable

---
+++ E_Design_Project_AirTable/gatsby-config.js
@@ -0,0 +1,40 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
+ */
+
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+module.exports = {
+  siteMetadata: {
+    title: `Design Shop`,
+    description: `Gatsby Airtable Example. Built using Airtable, Algolia Search, Gatsby Background Image plugin and  React Context API. Containts two sliders, real-time Airtable updates and submenus. Styled using Styled-Components. `,
+    author: `@johnsmilga`,
+    titleTemplate: `%s | Gatsby - Airtable`,
+    url: `https://gatsby-airtable-design-project.netlify.app/`,
+    image: `mainBcg.png`,
+    twitterUsername: `@john_smilga`,
+  },
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+  ],
+};
---
+++ E_Design_Project_AirTable/src/pages/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { graphql } from "gatsby";
+import {
+  Layout,
+  Hero,
+  About,
+  Projects,
+  Survey,
+  Slider,
+  GridProjects,
+} from "../components";
+
+const HomePage = () => {
+  return <h2>gatsby airtable starter</h2>;
+};
+
+export default HomePage;
