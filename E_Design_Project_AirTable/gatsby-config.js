2850bde16938c3c7946d4e99731aecae4e800a2c
danielOuattara <daniel.ouattara@gmx.com>
2023-10-27 15:53:45 +0200

    210 Connect Airtable API to Gatsby

--- E_Design_Project_AirTable/gatsby-config.js
+++ E_Design_Project_AirTable/gatsby-config.js
@@ -28,11 +28,32 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
+
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+
+    {
+      resolve: `gatsby-source-airtable`,
+      options: {
+        apiKey: process.env.GATSBY_AIRTABLE_API_KEY, // may instead specify via env, see below
+        concurrency: 5, // default, see using markdown and attachments for more information
+        tables: [
+          {
+            baseId: process.env.GATSBY_AIRTABLE_BASE_ID,
+            tableName: process.env.GATSBY_AIRTABLE_TABLE_NAME,
+            // tableView: `YOUR_TABLE_VIEW_NAME`, // optional
+            // queryName: `OPTIONAL_NAME_TO_IDENTIFY_TABLE`, // optionally default is false - makes all records in this table a separate node type, based on your tableView, or if not present, tableName, e.g. a table called "Fruit" would become "allAirtableFruit". Useful when pulling many airtables with similar structures or fields that have different types. See https://github.com/jbolda/gatsby-source-airtable/pull/52.
+            mapping: { image: `fileNode` }, // optional, e.g. "text/markdown", "fileNode"
+            // tableLinks: [`CASE`, `SENSITIVE`, `COLUMN`, `NAMES`], // optional, for deep linking to records across tables.
+            // separateNodeType: false, // boolean, default is false, see the documentation on naming conflicts for more information
+            // separateMapType: false, // boolean, default is false, see the documentation on using markdown and attachments for more information
+          },
+        ],
       },
     },
   ],
