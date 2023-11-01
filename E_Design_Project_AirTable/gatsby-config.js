6963b8a72fc8f19c4f150f164a0e9574af45408b
danielOuattara <daniel.ouattara@gmx.com>
2023-11-01 11:36:11 +0100

    correcting

--- E_Design_Project_AirTable/gatsby-config.js
+++ E_Design_Project_AirTable/gatsby-config.js
@@ -66,5 +66,27 @@ module.exports = {
         ],
       },
     },
+    {
+      // This plugin must be placed last in your list of plugins to ensure that it can query all the GraphQL data
+      resolve: `gatsby-plugin-algolia`,
+      options: {
+        appId: process.env.GATSBY_ALGOLIA_APP_ID,
+        // Use Admin API key without GATSBY_ prefix, so that the key isn't exposed in the application
+        // Tip: use Search API key with GATSBY_ prefix to access the service from within components
+        apiKey: process.env.GATSBY_ALGOLIA_ADMIN_KEY,
+        indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME, // for all queries
+        queries: require("./src/constants/algolia"),
+        chunkSize: 10000, // default: 1000
+        // settings: {
+        // optional, any index settings
+        // Note: by supplying settings, you will overwrite all existing settings on the index
+        // },
+        // mergeSettings: false, // optional, defaults to false. See notes on mergeSettings below
+        // concurrentQueries: false, // default: true
+        // dryRun: false, // default: false, only calculate which objects would be indexed, but do not push to Algolia
+        // continueOnFailure: false, // default: false, don't fail the build if Algolia indexing fails
+        // algoliasearchOptions: undefined, // default: { timeouts: { connect: 1, read: 30, write: 30 } }, pass any different options to the algoliasearch constructor
+      },
+    },
   ],
 };
