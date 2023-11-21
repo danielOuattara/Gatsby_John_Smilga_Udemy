431ded6911edc4445abb61e5a476797369b686c6
danielOuattara <daniel.ouattara@gmx.com>
2023-11-21 14:20:28 +0100

    191 MDX blockquote

--- D_MDX_Blog/root-mdx.jsx
+++ D_MDX_Blog/root-mdx.jsx
@@ -1,30 +1,12 @@
 import React from "react";
 import { MDXProvider } from "@mdx-js/react";
-import { Headings, Code } from "./src/components/Complete";
-
-// const my_h1 = (props) => (
-//   <span style={{ color: `orange` }} {...props}>
-//     A brand new heading h1
-//   </span>
-// );
-// const myH2 = (props) => {
-//   if (props.title) {
-//     return <h2 className="code" {...props} />;
-//   } else {
-//     <h2 className="code" {...props} />;
-//   }
-// };
-// const my_h3 = (props) => <h3 style={{ color: `pink` }} {...props} />;
-// const my_p = (props) => (
-//   <p style={{ fontSize: "18px", lineHeight: 1.6, color: "green" }} {...props} />
-// );
-
-const Testing = ({ children }) => <Code>{children}</Code>;
+import { Headings, Code, Blockquote } from "./src/components/Complete";
 
 const components = {
   h2: Headings.myH2,
   h4: Headings.myH4,
   inlineCode: Code,
+  blockquote: Blockquote,
 };
 
 export function wrapMDXRoot({ element }) {
