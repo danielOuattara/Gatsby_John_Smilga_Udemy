40952d95bcedb5e4749001c47d9e78f63dd06f33
danielOuattara <daniel.ouattara@gmx.com>
2023-11-21 15:16:56 +0100

    192 prism-react-renderer CONFIG

--- D_MDX_Blog/src/components/Complete/Example.jsx
+++ D_MDX_Blog/src/components/Complete/Example.jsx
@@ -1,26 +1,19 @@
 import React from "react";
 import { Highlight, themes } from "prism-react-renderer";
+export default function Example(props) {
+  //   console.log("props = ", props);
 
-const codeBlock = `
-const GroceryItem: React.FC<GroceryItemProps> = ({ item }) => {
+  const codeBlock = props.children.props.children.trim();
+  const language = props.children.props.className
+    .replace(/language-/, "")
+    .trim();
   return (
-    <div>
-      <h2>{item.name}</h2>
-      <p>Price: {item.price}</p>
-      <p>Quantity: {item.quantity}</p>
-    </div>
-  );
-}
-`;
-
-export default function Example() {
-  return (
-    <Highlight theme={themes.shadesOfPurple} code={codeBlock} language="tsx">
+    <Highlight theme={themes.oneDark} code={codeBlock} language={language}>
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
         <pre style={style}>
           {tokens.map((line, i) => (
             <div key={i} {...getLineProps({ line })}>
-              <span>{i + 1}</span>
+              {/* <span>{i + 1}</span> */}
               {line.map((token, key) => (
                 <span key={key} {...getTokenProps({ token })} />
               ))}
